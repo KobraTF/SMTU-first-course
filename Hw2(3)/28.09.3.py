f90bc4ba47eef6bd026e71374804471512d40bb3
KobraTF <115060064+KobraTF@users.noreply.github.com>
2022-11-11 16:00:15 +0300

    marking bagged programs

--- Hw2(3)/28.09.3.py
+++ Hw2(3)/28.09.3.py
@@ -10,12 +10,13 @@ def main():
         elif len(chrlist)!=0 and s[x] in closing:
             if chrlist[-1][1]==s[x]:
                 indexlist.append([chrlist[-1][0],x])
+                #print(chrlist)
                 chrlist.pop()
                 if len(indexlist)>1 and indexlist[-1][0]-1==indexlist[-2][1]:
                     indexlist[-2][1]=indexlist[-1][1]
                     indexlist.remove(indexlist[-1])
-        else:
-            chrlist=[]
+            else:
+                chrlist=[]
     mlist=[[x[1]-x[0],x] for x in indexlist]
     m=max(mlist)
     print(s[m[1][0]:m[1][1]+1])
--- Hw3(dead)/3_3.py
+++ Hw3(dead)/3_3.py
@@ -1,3 +1,6 @@
+from audioop import reverse
+
+
 def main():
     arr=list(*[[list(z) for z in list(set([tuple(sorted([s[i] for i in range(len(s)) if all ([char in s[i] and (len(s[n])==len(s[i])) and all([char2 in s[n] for char2 in (' '.join(s[i]).split())]) for char in (' '.join(s[n]).split())])])) for n in range(len(s))]))] for s in [[x[1:-1] for x in list(input()[1:-1].split(','))]]])
     for x in range(len(arr)):
