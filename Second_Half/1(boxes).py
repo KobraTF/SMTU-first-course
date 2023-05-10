d05232090f8b2437bb8cc964407c23cfbbf1bc5a
KobraTF <115060064+KobraTF@users.noreply.github.com>
2023-05-10 17:10:25 +0300

    working boxes with tests

--- Second_Half/1(boxes).py
+++ Second_Half/1(boxes).py
@@ -1,61 +1,112 @@
 from abc import ABC, abstractmethod
 from typing import List, Dict
+from random import randint
+from dataclasses import dataclass
 
-class Box(ABC):
-    storage:None
-
-    @abstractmethod
-    def add():
-        pass
-    @abstractmethod
-    def empty():
-        pass
-
-class Item():
-    name:str
-    value:int
-
-    def __init__(self,name:str,value:int) -> None:
-        self.name=name
-        self.values=value
-
-class ListBox(Box):
-    storage:List
-
-    def __init__(self):
-        self.storage=[]
-    def add(self,*items):
-        self.storage.extend(items)
-    def empty(self):
-        self.storage.clear()
-
-class DictBox(Box):
-    storage:Dict
-    counter:int
-
-    def __init__(self):
-        self.storage={}
-        self.counter=0
-    def add(self,*items):
-        for item in items:
-            self.storage.update(str(self.counter),item)
-            self.counter+=1
-    def empty(self):
-        self.storage.clear()
-
-def repack_boxes(*boxes:tuple(Box)):
-    all_items=[]
+def main():
     
-    for b in boxes:
-        if isinstance(b.storage,dict):
-            all_items.extend(b.storage.values())
-        else:
-            all_items.extend(b.storage)
-        b.empty()
-    
-    for _ in len(boxes):
-        quantity=len(boxes)
-        items_quant=len(all_items)
-        b=boxes.pop(0)
-        b.add([all_items.pop(0) for _ in range(items_quant//quantity)])
+    class Box(ABC):
+        storage:None
 
+        @abstractmethod
+        def add():
+            pass
+        @abstractmethod
+        def empty():
+            pass
+    
+    @dataclass
+    class Item:
+        name:str
+        value:int
+
+    class ListBox(Box):
+        storage:List
+
+        def __init__(self):
+            self.storage=[]
+        def add(self,*items:Item):
+            if items:
+                self.storage.extend(items)
+        def empty(self):
+            self.storage.clear()
+
+    class DictBox(Box):
+        storage:Dict
+        counter:int
+
+        def __init__(self):
+            self.storage={}
+            self.counter=1
+        def add(self,*items:Item):
+            
+            #print(items, 1 if items else 0)
+            if items:
+                for item in items:
+                    self.storage.update({str(self.counter):item})
+                    self.counter+=1
+        def empty(self):
+            self.storage.clear()
+            self.counter=1
+
+    def repack_boxes(*boxes):
+        all_items=[]
+        boxes=list(boxes)
+        #print('1111111111111111111111111',boxes)
+        if isinstance(boxes[0], list):
+            boxes=list(*boxes[0])
+        for b in boxes:
+            if isinstance(b.storage,dict):
+                all_items.extend(b.storage.values())
+            else:
+                all_items.extend(b.storage)
+            b.empty()
+            #print('aaaaaaaa')
+        
+        for _ in range(len(boxes)):
+            quantity=len(boxes)
+            items_quant=len(all_items)
+            b=boxes.pop(0)
+            b.add(*[all_items.pop(0) for _ in range(items_quant//quantity)])
+            #print('bbbbbbbbbb')
+    
+    listboxlist=[ListBox() for _ in range(randint(1,15))]
+    dictboxlist=[DictBox() for _ in range(randint(1,15))]
+    itemlist=[Item(str(randint(1,100)),i) for i in range(randint(0,150))]
+    #print(len)
+    lists=listboxlist+dictboxlist
+    #print(lists)
+    
+    for _ in range(len(lists)):
+        current:Box
+        current=lists.pop(0)
+        curitems=[]
+        if itemlist:
+            for _ in range(randint(0,5)):
+                if itemlist:
+                    curitems.append(itemlist.pop(0))
+            current.add(*curitems)
+    
+    lists=listboxlist+dictboxlist
+    print(f'{len(lists)} boxes before sorting:')
+    
+    for _ in range(len(lists)):
+        current:Box
+        current=lists.pop(0)
+        print(len(current.storage), end=' ')
+    print()
+    print()
+    lists=listboxlist+dictboxlist
+    print(f'{len(lists)} boxes after sorting:')
+    repack_boxes(*lists)
+    for _ in range(len(lists)):
+        current:Box
+        current=lists.pop(0)
+        print(len(current.storage),end=' ')
+    
+
+
+
+
+if __name__=="__main__":
+    main()
